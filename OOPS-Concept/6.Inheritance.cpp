ab11c2d4a5890c29844b615a8e6e745c3e2d848b
Bharat Gagan <bharatgagan07@gmail.com>
2026-03-26 00:30:07 +0530

    1. Inheritance

---
+++ OOPS-Concept/6.Inheritance.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+// Single Inheritance:
+
+class Person{
+    public:
+    string name;
+    int age;
+
+    Person(){
+        cout << "Parents Constructor" << endl;
+    }
+};
+
+class Student : public Person{
+    public:
+    int roll_no;
+
+    Student(){
+        cout << "Child Constructor" << endl;
+    }
+
+    void getInfo(){
+        cout << "Name: " << name << endl;
+        cout << "Roll No: " << roll_no << endl;
+    }
+};
+int main(){
+    Student s1;
+    s1.name = "Bharat";
+    s1.roll_no = 21;
+    s1.getInfo();
+    return 0;
+}
